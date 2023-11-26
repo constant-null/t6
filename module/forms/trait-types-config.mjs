191da5f459453b640e91e1ae4b7c1b23ff134153
Белоцкий Марк <m.belotskiy@corp.mail.ru>
2023-11-26 20:30:13 +0300

    Template for trait config

---
+++ module/forms/trait-types-config.mjs
@@ -0,0 +1,22 @@
+export default class TraitTypesConfig extends FormApplication {
+    static get defaultOptions() {
+        const options = super.defaultOptions;
+
+        options.template = "systems/t6/templates/forms/trait-types-config.html";
+        // options.classes = options.classes.concat(["t6"]);
+
+        return options;
+    }
+
+    getData() {
+        let data = super.getData()
+        data.traits = game.settings.get('t6', 'traitTypes');
+
+        return data
+    }
+
+    _updateObject(event, formData) {
+        // const data = expandObject(formData);
+        game.settings.set('t6', 'traitTypes', ['Personal', "History", "Troubles", "Equipment", "Cyberware"]  );
+    }
+}
--- module/t6.mjs
+++ module/t6.mjs
@@ -1,6 +1,7 @@
 import T6Actor from "./t6-actor.mjs";
 import T6Item from "./t6-item.mjs";
 import T6PCSheet from "./sheets/t6-pc-sheet.mjs";
+import TraitTypesConfig from "./forms/trait-types-config.mjs";
 
 Hooks.once("init", function () {
     console.log("T6 | Initializing T6 System");
@@ -14,6 +15,9 @@ Hooks.once("init", function () {
     CONFIG.Item.documentClass = T6Item;
     // Items.unregisterSheet("core", ItemSheet);
     // Items.registerSheet("efs", EFSItemSheet, { makeDefault: true, label: "EFS.Sheets.Item" })
+
+    preloadTemplates();
+    initializeAppConfig();
 });
 
 Hooks.on("renderCameraViews", function (app, html, data){
@@ -25,3 +29,27 @@ Hooks.on("renderPlayerList", function (app, html, data){
     const cameraGM = html.find("span:contains('GM')");
     cameraGM.text(cameraGM.text().replace('GM', game.i18n.localize("T6.Narrator")));
 });
+
+async function preloadTemplates() {
+    return loadTemplates([
+        "systems/t6/templates/parts/trait-list.html",
+    ]);
+}
+
+async function initializeAppConfig() {
+    game.settings.registerMenu('t6', 'traitTypesMenu', {
+        name: 'T6.Settings.TraitTypes.Name',
+        hint: 'T6.Settings.TraitTypes.Description',
+        label: "T6.Settings.TraitTypes.Label",
+        config: true,       // false if you dont want it to show in module config
+        type: TraitTypesConfig,
+        restricted: true
+    });
+
+    game.settings.register('t6', 'traitTypes', {
+        scope: 'world',     // "world" = sync to db, "client" = local storage
+        config: false,      // we will use the menu above to edit this setting
+        type: Array,
+        default: ['Personal', "History", "Troubles", "Equipment", "Cyberware"]        // can be used to set up the default structure
+    });
+}
