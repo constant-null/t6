4e638759ee45f65d60888ecbc0285f0f14362541
Mark Belotskiy <m.belotskiy@corp.mail.ru>
2023-12-20 01:15:47 +0300

    Fixed prototype token names, script for fixing them

---
+++ utils/fix-names.mjs
@@ -0,0 +1,24 @@
+import yaml from "js-yaml";
+import path from "path";
+import fs from "fs";
+
+const PACK_SRC = "./packs"
+
+const folders = fs.readdirSync(PACK_SRC, { withFileTypes: true }).filter(file =>
+    file.isDirectory()
+);
+
+for ( const folder of folders ) {
+    const srcFolder = path.join(PACK_SRC, folder.name, "_source");
+    console.log(`Searching Actors in source files ${folder.name}`);
+    fs.readdirSync(srcFolder, {}).forEach(filename => {
+        const filePath = path.join(srcFolder, filename);
+        const doc = yaml.load(fs.readFileSync(filePath, 'utf8'))
+        if (doc.prototypeToken) {
+            console.log(`Fixing names in Actor file ${filePath}`);
+            doc.prototypeToken.name = doc.name;
+            fs.writeFileSync(filePath, yaml.dump(doc)+"\n")
+        }
+    })
+}
+
